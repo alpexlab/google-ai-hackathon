acfbfe1a329cb19b7b906274f3502f4186a478ec
nik-55 <nik.xyz.in@gmail.com>
2024-09-24 00:43:40 +0530

    chore: rm db in docker

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -13,7 +13,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     os.environ.get("BASE_URL"),
-    f"{os.environ.get("BASE_URL")}:9100",
+    f'{os.environ.get("BASE_URL")}:9100',
 ]
 ALLOWED_HOSTS = ["*"]
 
